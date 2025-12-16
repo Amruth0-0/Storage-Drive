23b9d6489e73a56c501317bb224ada63837bc46a
Amruth0_0 <ammu.ventures@gmail.com>
2025-12-17 00:34:33 +0530

    Login Page with tokenization

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const userRouter = require('./routes/user.routes');
 const connectDB = require('./config/db');
 const {body, ValidationResult} = require('express-validator')
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 
 
 const app = express();
@@ -12,6 +13,7 @@ connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
+app.use(cookieParser());
 app.set('view engine', 'ejs');
 
 app.use('/user', userRouter);
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,6 +3,7 @@ const { body, validationResult } = require('express-validator');
 const router = express.Router();
 const userModel = require('../models/user.model');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 
 router.get('/register', (req,res)=>{
@@ -34,4 +35,51 @@ router.post('/register',
 
         }  
 )
+
+router.get('/login', (req,res)=>{
+    res.render('login');
+})
+
+router.post('/login', 
+    body('username').trim().isLength({min: 3}),
+    body('password').trim().isLength({min: 6}),
+
+    async (req,res)=> {
+        const err = validationResult(req);
+
+        if(!err.isEmpty()){
+            return res.status(400).json(
+                {errors: err.array(),
+                 message: "Invalid Data"
+                });
+            }
+
+        const {username, password} = req.body;
+
+        const user = await userModel.findOne({
+            username: username
+        });
+
+        if(!user){
+            return res.status(400).json(
+                {message: "Username or Password is incorrect"});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json(
+                {message: "Username or Password is incorrect"});
+        }    
+
+        const token = jwt.sign({
+            userId: user._id,
+            email: user.email,
+            username: user.username
+        }, process.env.JWT_SECRET);
+
+    res.cookie('token', token);
+    res.send('Logged In Successfully');
+
+    })
+
 module.exports = router;
