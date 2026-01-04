03f74f2c96fb52b358465b784a736aca063a8326
Amruth0_0 <ammu.ventures@gmail.com>
2026-01-04 17:47:46 +0530

    Authentication Middleware Added

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const upload = require("../config/multer.config");
+const fileModel = require("../models/files.models");
+const authMiddleware = require("../middleware/auth");
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/", authMiddleware, (req, res) => {
   res.render("home");
 });
 
-router.post("/upload", upload.single("file"), (req, res) => {
-  res.send(req.file);
+router.post("/upload", authMiddleware,  upload.single("file"), async (req, res) => {
+  const newFile = await fileModel.create({
+
+    path: req.file.path,
+    originalName: req.file.originalname,
+    user: req.user.userId
+
+  });
+  res.json(newFile);
 });
 
 module.exports = router;
